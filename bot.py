913a3f35deb7494e8843a7c8ba5d0fb6a0f5102b
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-02-10 21:50:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # (©)Codexbotz
 # Recode By Zaen @Mafia_Tobatz
+# Rehansaputradewantoro
 # Kalo clone Gak usah hapus 
 # gue tandain akun tele nya ngentod
 
@@ -53,7 +54,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ZoneDangerSex untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL2:
@@ -98,7 +99,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_CHANNEL4}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ZoneDangerSex untuk Bantuan"
                 )
                 sys.exit()
          if FORCE_SUB_CHANNEL5:
@@ -114,7 +115,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_CHANNEL5}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/ZoneDangerSex untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -128,13 +129,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/pantekyks untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/ZoneDangerSex untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/mahadappa"
+            f"[🔥 BERHASIL DIAKTIFKAN! FSUB5BOT BY Rehansaputradewantoro🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/ZoneDangerSex"
         )
         self.username = usr_bot_me.username
 
